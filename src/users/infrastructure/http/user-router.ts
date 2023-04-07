c4f145ed3f0ecdfb63f32d1d5c9bb0775a4fe0e3
Juancho997 <juanboninodev@gmail.com>
2023-04-07 00:21:00 -0300

    refactor: :art: added modules directory

--- src/users/infrastructure/http/user-router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { userController } from "../dependencies";
-
-const userRouter = Router();
-
-userRouter.get("/:id", userController.getById.bind(userController));
-
-export default userRouter;
