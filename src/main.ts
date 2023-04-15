88d837683ca672fc657fad3c51a69ad610174345
Juancho997 <juanboninodev@gmail.com>
2023-04-15 15:45:44 -0300

    feat: :sparkles: added routers importation and db connection

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,24 @@ import bodyParser from "body-parser";
 import express from "express";
 
 import { config } from "./config";
-import userRouter from "./users/infrastructure/http/user-router";
+import userRouter from "../src/modules/users/infrastructure/http/user-router";
+import productRouter from "../src/modules/products/infrastructure/http/product-router";
+import logger from "./utils/logger";
+import { CONNECTIONS } from "./database/connection-dependencies";
 
 function boostrap() {
   const app = express();
 
   app.use(bodyParser.json());
+
   app.use("/users", userRouter);
+  app.use("/products", productRouter);
 
   const { port } = config.server;
 
   app.listen(port, () => {
-    console.log(`[APP] - Starting application on port ${port}`);
+    logger.info(`[APP] - Starting application on port ${port}`);
+    CONNECTIONS.connectTo(process.env.DATABASE!)
   });
 }
 
